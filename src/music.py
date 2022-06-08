ad4dd61c8651132542245d528e5ade03d40d7441
bday-boy <asmith4642@gmail.com>
2022-06-07 18:53:53 -0700

    Added initial bot files

---
+++ src/music.py
@@ -0,0 +1,13 @@
+import discord
+import yt_dlp as yt
+
+YT_DLP_ARGS = {
+    'default_search': 'ytsearch',
+    'format': 'bestaudio/best',
+    'quiet': True,
+    'extract_flat': 'in_playlist'
+}
+
+
+class YouTubeVideo:
+    pass
