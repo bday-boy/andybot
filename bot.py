eef6f5b40ec5979e65d2eac6083702530842bbbb
bday-boy <asmith4642@gmail.com>
2022-06-08 21:26:11 -0700

    logging appends to discord.log now

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from src.utils.file import load_config
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(
-    filename='discord.log', encoding='utf-8', mode='w'
+    filename='discord.log', encoding='utf-8', mode='w+'
 )
 handler.setFormatter(logging.Formatter(
     '%(asctime)s:%(levelname)s:%(name)s: %(message)s'
