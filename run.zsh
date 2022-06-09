857807d461d81606467e97075bb2b0cef3bacbb9
bday-boy <asmith4642@gmail.com>
2022-06-08 20:06:41 -0700

    Changed run.sh to run.zsh

--- run.zsh
+++ run.zsh
@@ -4,4 +4,4 @@
 # solution because it loads all plugins and such as well
 source ~/.zshrc
 
-conda activate andybot && python ./bot.py >> python.log 2>&1 &
+conda activate andybot && python ./bot.py >> python.log 2>&1
