746522e570bbfe435700991d583e89b9d0be2d95
bday-boy <asmith4642@gmail.com>
2022-06-08 17:48:20 -0700

    Added run shell file

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/zsh
+
+# Necessary to expose conda to this script, but a bit of a lazy and bad
+# solution because it loads all plugins and such as well
+source ~/.zshrc
+
+conda activate andybot && python ./bot.py >> python.log 2>&1 &
