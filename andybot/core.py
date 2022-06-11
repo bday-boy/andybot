a18c2a185cf74965ad6037f9bf7e4078c44e3365
bday-boy <asmith4642@gmail.com>
2022-06-10 21:19:46 -0700

    Changed base to core, added exception handler

---
+++ andybot/core.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+from andybot.utils.file import cfg
+from andybot.utils.misc import hex_to_rgb
+
+
+class Andybot:
+    color = hex_to_rgb(cfg['color'])
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def format_embed(embed: discord.Embed) -> discord.Embed:
+        """Changes properties of an embed in-place and returns it to preserve
+        the fluid Embed chaining that the original class offers.
+        """
+        embed.color = discord.Color.from_rgb(*Andybot.color)
+        return embed
+
+
+async def handle_base_exceptions(ctx: commands.Context,
+                                 error: Exception) -> bool:
+    if isinstance(error, discord.HTTPException):
+        await ctx.send('Something went wrong with an HTTP request.')
+    elif isinstance(error, discord.ClientException):
+        await ctx.send('Something went wrong with the discord client.')
+    elif isinstance(error, discord.DiscordException):
+        await ctx.send('Something unknown went wrong with discord.')
+    await ctx.send(repr(error))
