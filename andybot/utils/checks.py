6bc53c58a58028a6aa0f66b21a4b6080e72ff6ad
bday-boy <asmith4642@gmail.com>
2022-06-13 19:43:17 -0700

    Added docstrings

--- andybot/utils/checks.py
+++ andybot/utils/checks.py
@@ -2,11 +2,13 @@ from discord.ext import commands
 
 
 def in_voice_call(ctx: commands.Context) -> bool:
+    """Checks if the client is in a voice call."""
     voice = ctx.guild.voice_client
     return voice and voice.channel
 
 
 async def in_voice_call_check(ctx: commands.Context) -> bool:
+    """Checks if the client is in a voice call but, ~asynchronously~ :D"""
     if in_voice_call(ctx):
         return True
     else:
@@ -15,11 +17,13 @@ async def in_voice_call_check(ctx: commands.Context) -> bool:
 
 
 def is_playing_audio(ctx: commands.Context) -> bool:
+    """Checks if the client is playing audio."""
     voice = ctx.guild.voice_client
     return in_voice_call(ctx) and voice.source
 
 
 async def is_playing_audio_check(ctx: commands.Context) -> bool:
+    """Checks if the client is playing audio, but ~asynchronously~ :D"""
     if is_playing_audio(ctx):
         return True
     else:
