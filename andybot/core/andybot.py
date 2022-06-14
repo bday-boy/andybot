52fda4f27d3f85cc172776181d2409a3ae4a0191
bday-boy <asmith4642@gmail.com>
2022-06-14 14:02:31 -0700

    Added kwargs to format_embed

--- andybot/core/andybot.py
+++ andybot/core/andybot.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import discord
 from discord.ext import commands
 
@@ -12,10 +14,13 @@ class Andybot:
         pass
 
     @staticmethod
-    def format_embed(embed: discord.Embed) -> discord.Embed:
+    def format_embed(embed: Optional[discord.Embed] = None,
+                     **kwargs) -> discord.Embed:
         """Changes properties of an embed in-place and returns it to preserve
         the fluid Embed chaining that the original class offers.
         """
+        if embed is None:
+            embed = discord.Embed(**kwargs)
         embed.color = discord.Color.from_rgb(*Andybot.color)
         return embed
 
