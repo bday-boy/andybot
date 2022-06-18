f486aed5f02adb491b99d6a2648825bbdd77a25e
bday-boy <asmith4642@gmail.com>
2022-06-18 01:59:48 -0700

    Added scatterplot for damage

--- andybot/cogs/pokemon/core/math.py
+++ andybot/cogs/pokemon/core/math.py
@@ -1,5 +1,8 @@
+import random
 from math import floor, ceil
 from typing import List, Tuple
+
+import numpy as np
 
 MIN_IVS = 0
 MAX_IVS = 31
@@ -27,26 +30,58 @@ def base_dmg(level: int, power: int, atk: int, def_: int) -> int:
     return floor(numerator / 50) + 2
 
 
-def calc_dmg(level: int, power: int, atk: int, def_: int, other: int = 1,
+def random_dmg_array(level: int, power: int, atk: int, def_: int, other: int = 1,
              random_num: float = 1.0) -> int:
     """Calculates the damage done from one Pokemon to another. random_num is
     a float in the range [0.85, 1] that defaults to 1 for maximum damage.
 
     Damage formula details: https://bulbapedia.bulbagarden.net/wiki/Damage
     """
-    return floor(base_dmg(level, power, atk, def_) * other * random_num)
+    base = base_dmg(level, power, atk, def_)
+    return floor(base * other * random_num)
+
+
+def base_dmg_vector(level: int, power: int, atk: int, def_vec: np.ndarray
+                    ) -> np.ndarray:
+    """Calculates the base damage part of the damage formula as a vector."""
+    level_modifier = floor((2 * level / 5) + 2)
+    numerator = np.floor(level_modifier * power * atk / def_vec)
+    return np.floor(numerator / 50) + 2
+
+
+def calc_dmg_vector(level: int, power: int, atk: int, def_vec: np.ndarray,
+                    other: int = 1, random_num: float = 1.0) -> np.ndarray:
+    """Calculates the damage done from one Pokemon to another as a vector."""
+    base = base_dmg_vector(level, power, atk, def_vec)
+    return np.floor(base * other * random_num)
+
+
+def calc_dmg_vector_random(level: int, power: int, atk: int,
+                           def_vec: np.ndarray, other: int = 1
+                           ) -> np.ndarray:
+    """Calculates the damage done from one Pokemon to another as a vector."""
+    base = base_dmg_vector(level, power, atk, def_vec)
+    return np.floor(base * other * (random.randint(85, 100) / 100))
 
 
 def highest_dmg(level: int, power: int, atk: int, def_: int, other: int = 1
                 ) -> int:
     """Calculates the highest damage roll based on the given inputs."""
-    return calc_dmg(level, power, atk, def_, other, 1.0)
+    return random_dmg_array(level, power, atk, def_, other, 1.0)
 
 
 def lowest_dmg(level: int, power: int, atk: int, def_: int, other: int = 1
                ) -> int:
     """Calculates the highest damage roll based on the given inputs."""
-    return calc_dmg(level, power, atk, def_, other, 0.85)
+    return random_dmg_array(level, power, atk, def_, other, 0.85)
+
+
+def random_nature() -> float:
+    """There are 25 natures total. Each stat has 4 natures that raise a given
+    stat by 1.1x, 4 natures that lower by 1.1x, and 17 natures that don't
+    affect it.
+    """
+    return random.choices((0.9, 1, 1.1), cum_weights=(4, 21, 25), k=1)
 
 
 def inner_stat_formula(base: int, IV: int, EV: int, level: int) -> int:
@@ -64,64 +99,114 @@ def inner_stat_formula(base: int, IV: int, EV: int, level: int) -> int:
 
 def hp_stat(base: int, IV: int, EV: int, level: int) -> int:
     """Calculates a Pokemon's in-game HP stat.
-    
+
     Formula seen here: https://pokemon.fandom.com/wiki/Statistics
     """
     return inner_stat_formula(base, IV, EV, level) + level + 10
 
 
 def non_hp_stat(base: int, IV: int, EV: int, level: int,
-               nature: float = 1.0) -> int:
+                nature: float = 1.0) -> int:
     """Calculates a Pokemon's in-game non-HP stat.
-    
+
     Formula seen here: https://pokemon.fandom.com/wiki/Statistics
     """
     return floor((inner_stat_formula(base, IV, EV, level) + 5) * nature)
 
 
-def lowest_hp_stat(base: int, level: int) -> int:
-    """Calculates the lowest HP stat value at the given level."""
-    return hp_stat(base, 0, 0, level)
+def hp_stat_extrema(base: int, level: int) -> Tuple[int, int]:
+    return (
+        hp_stat(base, MIN_IVS, MIN_EVS, level),
+        hp_stat(base, MAX_IVS, MAX_EVS, level)
+    )
 
 
-def lowest_non_hp_stat(base: int, level: int, nature: float = 1.0) -> int:
-    """Calculates the lowest non-HP stat value at the given level."""
-    return non_hp_stat(base, 0, 0, level, nature)
-
-
-def highest_hp_stat(base: int, level: int) -> int:
-    """Calculates the highest HP stat value at the given level."""
-    return hp_stat(base, 31, 252, level)
-
-
-def highest_non_hp_stat(base: int, level: int, nature: float = 1.0) -> int:
-    """Calculates the highest non-HP stat value at the given level. Nature
-    is neutral by default.
-    """
-    return non_hp_stat(base, 31, 252, level, nature)
-
-
-def lowest_and_highest_hp_stat(base: int, level: int) -> Tuple[int, int]:
-    return lowest_hp_stat(base, level), highest_hp_stat(base, level)
-
-
-def lowest_and_highest_non_hp_stat(base: int, level: int) -> Tuple[int, int]:
-    return lowest_non_hp_stat(base, level), highest_non_hp_stat(base, level)
+def non_hp_stat_extrema(base: int, level: int, nature: float = 1.0
+                        ) -> Tuple[int, int]:
+    return (
+        non_hp_stat(base, MIN_IVS, MIN_EVS, level, nature),
+        non_hp_stat(base, MAX_IVS, MAX_EVS, level, nature)
+    )
 
 
 def hp_stat_range(base: int, level: int) -> int:
-    lowest_hp, highest_hp = lowest_and_highest_hp_stat(base, level)
+    lowest_hp, highest_hp = hp_stat_extrema(base, level)
     return list(range(lowest_hp, highest_hp + 1))
 
 
 def non_hp_stat_range(base: int, level: int, nature: float = 1.0) -> int:
-    lowest_non_hp, highest_non_hp = lowest_and_highest_non_hp_stat(base, level)
+    lowest_non_hp, highest_non_hp = non_hp_stat_extrema(base, level)
     return list(range(lowest_non_hp, highest_non_hp + 1))
 
 
 def full_stat_ranges(bases: List[int], level: int, nature: float = 1.0
                      ) -> List[int]:
-    stats = [lowest_and_highest_hp_stat(bases[0], level)]
+    stats = [hp_stat_extrema(bases[0], level)]
     for base in bases[1:]:
-        stats.append(lowest_and_highest_non_hp_stat(base, level, nature))
+        stats.append(non_hp_stat_extrema(base, level, nature))
     return stats
+
+###############################################################################
+############################### Numpy versions ################################
+###############################################################################
+
+default_size = 200
+
+
+def random_nature_array(size: int = default_size) -> np.ndarray:
+    p = np.array((4, 17, 4)) / 25
+    natures = np.random.choice((0.9, 1.0, 1.1), size=size, p=p)
+    return natures
+
+
+def random_ev_array(size: int = default_size) -> np.ndarray:
+    evs = np.random.random_integers(0, 252, size=size)
+    return evs
+
+
+def random_iv_array(size: int = default_size) -> np.ndarray:
+    ivs = np.random.random_integers(0, 31, size=size)
+    return ivs
+
+
+def random_inner_stat_array(base: int, level: int, size: int = default_size 
+                            ) -> np.ndarray:
+    """Creates an array of random stats given a base. Assumes 0 EVs."""
+    random_ivs = random_iv_array(size)
+    return np.floor((2 * base + random_ivs) * level / 100)
+
+
+def random_stat_array(base: int, level: int, size: int = default_size
+                      ) -> np.ndarray:
+    natures = random_nature_array(size)
+    base_stats = random_inner_stat_array(base, level, size)
+    return np.floor((base_stats + 5) * natures)
+
+
+def base_dmg_array(level: int, power: int, atk: int,
+                   def_array: np.ndarray) -> np.ndarray:
+    """Calculates the base damage part of the damage formula. This is the
+    part before all the external modifiers are applied. Technically, some
+    games apply modifiers before the +2 is added, but the difference is
+    negligible enough that it is outside the scope of this bot.
+    """
+    level_modifier = floor((2 * level / 5) + 2)
+    numerator = np.floor(level_modifier * power * atk / def_array)
+    return np.floor(numerator / 50) + 2
+
+
+def random_dmg_array(level: int, power: int, atk: int, def_array: np.ndarray,
+                     other: float = 1.0) -> int:
+    """Calculates the damage done from one Pokemon to another. random_num is
+    a float in the range [0.85, 1] that defaults to 1 for maximum damage.
+
+    Damage formula details: https://bulbapedia.bulbagarden.net/wiki/Damage
+    """
+    array_size = def_array.shape[0]
+    base = base_dmg_array(level, power, atk, def_array)
+    random_array = np.random.random_integers(85, 100, size=array_size) / 100
+    return np.floor(base * other * random_array)
+
+
+def damage_as_hp_percent(dmg_array: np.ndarray, hp: int) -> np.ndarray:
+    return 100 * dmg_array / hp
--- andybot/cogs/pokemon/core/plotter.py
+++ andybot/cogs/pokemon/core/plotter.py
@@ -1,43 +1,29 @@
-from typing import Tuple
 import matplotlib.pyplot as plt
 import numpy as np
 
 import andybot.cogs.pokemon.core.math as pokemath
 
 
-def dmg_array(level: int, power: int, atk: int, def_range: Tuple[int],
-              stab: bool, type_: float, mods: float, roll: float,
-              hp: int) -> np.ndarray:
-    full_modifier = (1.5 if stab else 1) * type_ * mods
-    dmgs = np.asarray([
-        pokemath.calc_dmg(level, power, atk, d, full_modifier, roll)
-        for d in def_range
-    ])
-    return 100 * dmgs / hp
-
-
-def plot_damage(attacker_level: int, defender_level: int, power: int, atk: int,
-                def_: int, stab: bool, type_: float, mods: float, hp: int
-                ) -> None:
-    """Creates a simple 2D graph."""
+def damage_scatter(attacker_level: int, defender_level: int, power: int,
+                   atk: int, base_def: int, stab: bool, type_: float,
+                   mods: float, hp: int) -> None:
+    """Creates a simple 2D graph of the possible damage done to a defending
+    Pokemon.
+    """
     fig, ax = plt.subplots(figsize=(5, 2.7), layout='constrained')
-    def_range = pokemath.non_hp_stat_range(def_, defender_level)
-    lowest_dmgs = dmg_array(
-        attacker_level, power, atk, def_range, stab, type_, mods, 0.85, hp
-    )
-    highest_dmgs = dmg_array(
-        attacker_level, power, atk, def_range, stab, type_, mods, 1, hp
-    )
-    ax.fill_between(def_range, lowest_dmgs, highest_dmgs, alpha=0.25)
-    ax.plot(def_range, highest_dmgs, label='Highest roll')
-    ax.plot(def_range, lowest_dmgs, label='Lowest roll')
+    def_vec = pokemath.random_stat_array(base_def, defender_level)
+    total_mods = (1.5 if stab else 1) * type_ * mods
+    dmgs = pokemath.random_dmg_array(attacker_level, power, atk, def_vec,
+                                     total_mods)
+    hp_percent = pokemath.damage_as_hp_percent(dmgs, hp)
+    ax.scatter(def_vec, hp_percent, label='Damages')
     ax.set_xlabel('Defense')
     ax.set_ylabel('Damage (as % of hp)')
     ax.set_title('Damage calculation')
     ax.legend()
     plt.grid()
-    plt.savefig('./attachments/damage.png')
+    plt.savefig('./attachments/damage_scatter.png')
 
 
 if __name__ == '__main__':
-    plot_damage(100, 50, 120, 389, 75, False, 2, 2, 165)
+    damage_scatter(50, 50, 90, 134, 80, True, 1, 1.5, 165)
